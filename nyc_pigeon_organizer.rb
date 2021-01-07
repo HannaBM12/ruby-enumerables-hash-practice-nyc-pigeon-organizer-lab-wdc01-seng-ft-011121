92ca0cdaac4d3f3c618fc5dbf0cb28cd4fe689db
Hanna Mulugeta <hannabmulugeta@gmail.com>
2021-01-07 01:05:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,11 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}){|key, value, new_data| }
-  binding.pry
+  hash.each_with_object({}) do |(k, v), a|
+ a[k] = v + 3
+end
+  data.each_with_object({}){|key, value, new_data|
+
+    binding.pry
+  }
 
 end
