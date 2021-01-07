8839ff9cd61db0333fcf6d56a6554dc1a598763a
Hanna Mulugeta <hannabmulugeta@gmail.com>
2021-01-07 02:03:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
 
-  accumulator = data.each_with_object({}){|(key, value), new_data|
+  data.each_with_object({}){|(key, value), new_data|
     value.map {|inner_key, inner_value|
       inner_value.map{|name|
         unless new_data[name]
@@ -16,6 +16,6 @@ def nyc_pigeon_organizer(data)
       }
     }
   }
-  binding.pry
+  # binding.pry
 
 end
