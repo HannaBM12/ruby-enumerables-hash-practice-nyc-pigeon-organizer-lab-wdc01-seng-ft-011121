34ecc6a2dc5917b3c1cfbd175e953fcec58c223f
Hanna Mulugeta <hannabmulugeta@gmail.com>
2021-01-07 01:13:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}){|(key, value), new_data|
     value.map {|inner_key, inner_value|
       inner_value.map{|name|
+        new_data[name] = {}
         binding.pry
       }
       binding.pry
